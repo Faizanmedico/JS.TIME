b32c4acc89eb793f5c6474f0b046a2db1a270b5d
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2024-01-08 22:50:12 +0500

    A script on this page starts this clock:

---
+++ script.js
@@ -0,0 +1,8 @@
+
+            
+   setInterval(myTimer, 1000);
+
+   function myTimer() {
+     const d = new Date();
+     document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+   }      
